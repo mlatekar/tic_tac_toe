c75c9fcd370e09b06208aaa266b431ba6e318a76
mangesh <mlatekar@gmail.com>
2020-02-04 12:07:09 +0530

    ADDED:Resetting the board for fresh start

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -1,1 +1,21 @@
 echo "Welcome to Tic Tac Toe Game"
+
+#CONSTANT
+ROW=3
+COLUMN=3
+
+#DECLARE ARRAY
+declare -a board
+
+#RESET THE BOARD
+function reset()
+{
+for (( i=0; i<$ROW; i++ ))
+do
+	for(( j=0; j<$COLUMN; j++ ))
+	do
+		board[$i,$j]=" "
+	done
+done
+}
+reset
