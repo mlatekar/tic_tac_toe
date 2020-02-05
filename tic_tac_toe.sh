f7cc6b520e3418d74c7f9d3161994f558a065af0
mangesh <mlatekar@gmail.com>
2020-02-05 18:30:29 +0530

    ADDED:check with toss who play first

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -7,9 +7,8 @@ COLUMN=3
 #VARIABLE
 player=0
 
-
 #DECLARE ARRAY
-declare -a board
+declare -A board
 
 #RESET THE BOARD
 function reset()
@@ -18,20 +17,50 @@ function reset()
 	do
 		for(( j=0; j<$COLUMN; j++ ))
 		do
-			board[$i,$j]=" "
+			board[$i,$j]="-"
 		done
 	done
 }
 
+#ASSIGN LETTER TO PLAYER
 function assignSymbol()
 {
-		if [ $((RANDOM%2)) -eq  0 ]
+	if [ $((RANDOM%2)) -eq  0 ]
 		then
 			player="X"
+			computer="O"
 		else
 			player="O"
+			computer="X"
 		fi
 	echo "player has assign : $player"
 }
+
+#TOSS FOR WHO WILL PLAY FIRST
+function tossForPlay()
+{
+	if [ $((RANDOM%2)) -eq 0 ]
+	then
+		echo "player play first"
+	else
+		echo "computer play first"
+	fi
+}
+
+#DISPLAY THE TIC-TAC-TOE BOARD
+function displayTheBoard()
+{
+   for (( i=0; i<$ROW; i++ ))
+   do
+      echo "-------------"
+      for (( j=0; j<$COLUMN; j++ ))
+      do
+         echo -n "| ${board[$i,$j]} "
+      done
+      echo "|"
+   done  
+}
 reset
 assignSymbol
+tossForPlay
+displayTheBoard
